0cc886a86dac2516475ea8b7552c1a8a52d5b888
Ashutosh Mishra <mishratushar388@gmail.com>
2024-03-06 20:37:28 +0530

    fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -29,7 +29,7 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-acoount").patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserProfileImage)
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 
 router.route("/channel/:username").get(verifyJWT,getUserChannelProfile)
 router.route("/history").get(verifyJWT,getWatchHistory)
